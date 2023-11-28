fc416b39edb2ccbd0dc071de812d9764d1218d9a
morpheusbackend <morpheusbackend@gmail.com>
2023-11-28 15:42:23 -0600

    pasmd

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from "express";
+const fs = require('fs')
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.json({ limit: '10000kb', extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
